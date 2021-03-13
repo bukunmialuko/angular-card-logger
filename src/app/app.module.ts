dcf7c18c911ffbe4469ad2c2ce544732e54a8b21
bukunmialuko <oluwabukunmi.aluko@gmail.com>
2021-03-13 22:21:15 +0100

    Bootstrapping

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardsListComponent } from './cards-list/cards-list.component';
+import { CardItemComponent } from './card-item/card-item.component';
+import { CardFormComponent } from './card-form/card-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardsListComponent,
+    CardItemComponent,
+    CardFormComponent
   ],
   imports: [
     BrowserModule,
